a91b4d3b272022b0a4bbac2dbc659e940d1cecbc
imanol309 <imanolguzman309@gmail.com>
2022-01-31 17:04:18 -0400

    first commit

---
+++ forRestar.js
@@ -0,0 +1,8 @@
+function ForRestar(a) {
+    for (let p = 0; p < a.length; p-1) {
+        const element = a[p];
+        console.log(element)
+    }
+}
+
+ForRestar(20)
---
+++ sumar.js
@@ -0,0 +1,7 @@
+function sumasPorDos(a,b) {
+    const ResultadoSuma =  a + b
+    const multiplicarPorDos = ResultadoSuma * 2
+    console.log(multiplicarPorDos)
+}
+
+sumasPorDos(5,5)
