bb684853947f781831249b8fb8ebe21fed10bfba
imanol309 <imanolguzman309@gmail.com>
2022-03-01 15:10:14 -0400

    Diferentes tipos de datos

--- python/traingOne.py
+++ python/traingOne.py
@@ -1,2 +1,18 @@
 #Esto imprime por pantalla
 print('Hola imanol')
+print (type(25))  
+print (type(False))
+print (type('perez'))
+print('Imanol ' + 'Juan ' + 'Oscar')
+
+#Entero
+print(type(30))
+
+# Float
+print (type(30.5))
+
+#list
+
+[10,25,85,122]
+['Imanol', 'PERRO', 'Gato']
+[10, 'Guzman', True, 10.25]
